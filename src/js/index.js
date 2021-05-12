69815b35354968bcb514e54691e0b02b02e20a22
zlic <161250197@smail.nju.edu.cn>
2021-05-12 15:03:53 +0800

    🦄 refactor(画布管理): get 方法 和 init 方法重构

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
 // 入口文件
 
 import '../style/style.less';
-import { initCanvasSize } from './util/canvas-manager';
+import { initCanvasManager } from './util/canvas-manager';
 import { initCoverManager } from './util/cover-manager';
 import { initDotSetting } from './util/dot-setting';
 
-initCanvasSize();
+initCanvasManager();
 initDotSetting();
 initCoverManager();
--- src/js/util/canvas-manager.js
+++ src/js/util/canvas-manager.js
@@ -1,49 +1,36 @@
 // 画布管理
 
-let canvasWrapperElement;
-
 let getCanvasWrapperElement = function () {
-    canvasWrapperElement = document.getElementById('canvas-wrapper');
+    const canvasWrapperElement = document.getElementById('canvas-wrapper');
     getCanvasWrapperElement = function () {
         return canvasWrapperElement;
     };
     return canvasWrapperElement;
 };
 
-let upCanvasElement;
-
 /**
  * 获取上层画布节点
  * @returns {HTMLCanvasElement}
  */
 let getUpCanvasElement = function () {
-    upCanvasElement = document.getElementById('up-canvas');
+    const upCanvasElement = document.getElementById('up-canvas');
     getUpCanvasElement = function () {
         return upCanvasElement;
     };
     return upCanvasElement;
 };
 
-let downCanvasElement;
-
 /**
  * 获取下层画布节点
  * @returns {HTMLCanvasElement}
  */
 let getDownCanvasElement = function () {
-    downCanvasElement = document.getElementById('down-canvas');
+    const downCanvasElement = document.getElementById('down-canvas');
     getDownCanvasElement = function () {
         return downCanvasElement;
     };
     return downCanvasElement;
 };
-
-/**
- * 初始化画布尺寸
- */
-function initCanvasSize () {
-    setTimeout(updateCanvasSize);
-}
 
 function updateCanvasSize () {
     const canvasWrapper = getCanvasWrapperElement();
@@ -66,8 +53,15 @@ function setCanvasSize ({ width, height }) {
     });
 }
 
+/**
+ * 初始化画布管理
+ */
+function initCanvasManager () {
+    setTimeout(updateCanvasSize);
+}
+
 export {
-    initCanvasSize,
+    initCanvasManager,
     getDownCanvasElement,
     getUpCanvasElement
 };
