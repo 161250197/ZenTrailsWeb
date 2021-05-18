97cfdf3f855ee8fdb44744db4e92e3a3ef1c0451
zlic <161250197@smail.nju.edu.cn>
2021-05-18 22:43:23 +0800

    🦄 refactor(画布): 抽象刷新画布方法

--- src/js/cartoon/index.js
+++ src/js/cartoon/index.js
@@ -20,6 +20,7 @@ import {
     showElement
 } from '../util/base';
 import { getCoverElement } from '../cover';
+import { refreshCanvas } from '../canvas';
 
 let __isPlayingCartoon = false;
 let __updateCartoonHandle;
@@ -105,41 +106,6 @@ function stopCartoon ({ key }) {
         cancelAnimationFrame(__updateCartoonHandle);
         resetPosition();
         requestAnimationFrame(refreshCanvas);
-    }
-}
-
-function refreshCanvas () {
-    getDownCanvasDrawHelper().clearCanvas();
-    getUpCanvasDrawHelper().clearCanvas();
-    const pathArr = getPathArr();
-    pathArr.forEach(path => drawPath(path));
-}
-
-function drawPath (path) {
-    const downCanvasDrawHelper = getDownCanvasDrawHelper();
-    const upCanvasDrawHelper = getUpCanvasDrawHelper();
-    upCanvasDrawHelper.resetColor();
-    upCanvasDrawHelper.drawCircle(path.firstDot);
-    let dots = [path.firstDot];
-    while (dots.length)
-    {
-        let newDots = [];
-        for (const dot of dots)
-        {
-            newDots = newDots.concat(dot.followDots);
-        }
-        dots = newDots;
-        for (const dot of dots)
-        {
-            const { color, isAntiClockwise, lastDot } = dot;
-            upCanvasDrawHelper.setColor(color);
-            upCanvasDrawHelper.drawCircle(dot);
-            downCanvasDrawHelper.drawLine(lastDot, dot);
-            if (isAntiClockwise)
-            {
-                upCanvasDrawHelper.drawCircle(dot, 5);
-            }
-        }
     }
 }
 
