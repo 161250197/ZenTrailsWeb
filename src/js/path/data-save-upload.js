db26584910422528f7c0654f4ef5ec98b260e850
zlic <161250197@smail.nju.edu.cn>
2021-05-22 22:11:36 +0800

    ✨ feat(导出导入数据): 完成导入数据解析

--- src/js/path/data-save-upload.js
+++ src/js/path/data-save-upload.js
@@ -16,6 +16,7 @@ import {
     setPathArr
 } from '.';
 import { refreshCanvas } from '../canvas';
+import { Path } from './data-structure';
 
 let __getDataUploadBtnElement = createSingletonFunc(
     function () {
@@ -47,10 +48,75 @@ const __uploadData = (function () {
     const reader = new FileReader();
     reader.onload = function () {
         const pathArr = (function () {
-            // TODO
+            const __pathArr = [];
             const dataStr = __decodeFileContent(reader.result);
-            console.log(dataStr.split('\n'));
-            return [];
+            const [
+                idToFollowIdsStr,
+                firstIdDotStr,
+                followIdDotStr
+            ] = dataStr.split('\n');
+            const idToFollowIdsMap = (function () {
+                const map = {};
+                const idToFollowIdsArr = idToFollowIdsStr
+                    .split(';')
+                    .map(str => {
+                        const [id, followIdsStr] = str.split(' ');
+                        const followIds = followIdsStr.split(',');
+                        return { id, followIds };
+                    });
+                for (const { id, followIds } of idToFollowIdsArr)
+                {
+                    map[id] = followIds;
+                }
+                return map;
+            }());
+            const firstIdDotArr = firstIdDotStr
+                .split(';')
+                .map(str => {
+                    const [id, x, y] = str.split(' ');
+                    const path = new Path({ x, y });
+                    __pathArr.push(path);
+                    return { id, dot: path.firstDot };
+                });
+            const idToFollowDotDataMap = (function () {
+                const map = {};
+                const idToFollowDataArr = followIdDotStr
+                    .split(';')
+                    .map(str => {
+                        const [id, x, y, angleVelocityValue, isAntiClockwiseValue, color] = str.split(' ');
+                        const angleVelocity = Number(angleVelocityValue);
+                        const isAntiClockwise = !!isAntiClockwiseValue;
+                        return { id, x, y, angleVelocity, isAntiClockwise, color };
+                    });
+                for (const { id, x, y, angleVelocity, isAntiClockwise, color } of idToFollowDataArr)
+                {
+                    map[id] = { x, y, angleVelocity, isAntiClockwise, color };
+                }
+                return map;
+            }());
+            let idDotArr = firstIdDotArr;
+            while (idDotArr.length)
+            {
+                let newIdDotArr = [];
+                for (const { id, dot } of idDotArr)
+                {
+                    const followIds = idToFollowIdsMap[id];
+                    if (followIds)
+                    {
+                        for (const followId of followIds)
+                        {
+                            const { x, y, angleVelocity, isAntiClockwise, color } = idToFollowDotDataMap[followId];
+                            const followDot = dot.appendDot({ x, y });
+                            followDot.angleVelocity = angleVelocity;
+                            followDot.isAntiClockwise = isAntiClockwise;
+                            followDot.color = color;
+                            newIdDotArr.push({ id: followId, dot: followDot });
+                        }
+                    }
+                }
+                idDotArr = newIdDotArr;
+            }
+            return __pathArr;
         }());
         setPathArr(pathArr);
         refreshCanvas();
@@ -70,26 +136,26 @@ function __saveData () {
             getFollowIdDotArr
         } = (function () {
             let id = 0;
-            const idFirstDotArr = [];
-            const idFollowDotArr = [];
+            const firstIdDotArr = [];
+            const followIdDotArr = [];
             return {
                 createFirstIdDot: function (dot) {
                     const idDot = { id, dot };
-                    idFirstDotArr.push(idDot);
+                    firstIdDotArr.push(idDot);
                     id++;
                     return idDot;
                 },
                 getFirstIdDotArr: function () {
-                    return idFirstDotArr;
+                    return firstIdDotArr;
                 },
                 createFollowIdDot: function (dot) {
                     const idDot = { id, dot };
-                    idFollowDotArr.push(idDot);
+                    followIdDotArr.push(idDot);
                     id++;
                     return idDot;
                 },
                 getFollowIdDotArr: function () {
-                    return idFollowDotArr;
+                    return followIdDotArr;
                 }
             };
         }());
@@ -136,7 +202,7 @@ function __saveData () {
             return getFollowIdDotArr()
                 .map(({ id, dot }) => {
                     const { x, y, angleVelocity, isAntiClockwise, color } = dot;
-                    return `${ id } ${ x } ${ y } ${ angleVelocity } ${ isAntiClockwise ? 1 : 0 } ${ color }`;
+                    return `${ id } ${ x } ${ y } ${ angleVelocity } ${ isAntiClockwise ? '1' : '' } ${ color }`;
                 })
                 .join(';');
         }());
